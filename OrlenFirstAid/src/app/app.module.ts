90f53d8abbe3f5e45ab70fecfbe9d88897e81426
Monika Junik <monica.junik@gmail.com>
2019-09-15 01:29:34 +0200

    scss, home screen, header, first-aid-question

--- OrlenFirstAid/src/app/app.module.ts
+++ OrlenFirstAid/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstAidQuestionComponent } from './first-aid-question/first-aid-question.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstAidQuestionComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
