232f00ad4e870aedd795e18dfc4f20e7258dae1f
martagor11 <martagor1993@o2.pl>
2019-09-15 03:36:39 +0200

    add content to next page without styling

--- OrlenFirstAid/src/app/secure/secure.component.ts
+++ OrlenFirstAid/src/app/secure/secure.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-secure',
   templateUrl: './secure.component.html',
-  styleUrls: ['./secure.component.css']
+  styleUrls: ['./secure.component.scss']
 })
 export class SecureComponent implements OnInit {
+
+  title: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Zabezpiecz miejsce wypadku';
   }
 
 }
